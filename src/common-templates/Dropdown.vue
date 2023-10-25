4d80c191839e9060606b81b7cfc23cdcecb74ddc
Martin Rusev <marto260503@abv.bg>
2023-10-25 13:26:53 +0300

    Register user successfully

--- src/common-templates/Dropdown.vue
+++ src/common-templates/Dropdown.vue
@@ -1,15 +1,27 @@
 <template>
   <div>
-    <select title="dropdown" v-model=selectedValue>
-      <option v-for="zone in zones" :key="zone" value="zone">{{ zone }}</option>
+    <select v-model="selectedValue" title="dropdown" @input="$emit('update:modelValue', $event.target.value)" :value="modelValue">
+      <option v-for="zone in zones" :key="zone" :value="zone">{{ zone }}</option>
     </select>
   </div>
 </template>
 
 <script setup>
 import moment from "moment-timezone";
+defineEmits(['update:modelValue'])
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    required:true
+  }
+})
 
 const zones = moment.tz.names();
 
 const selectedValue = '';
+const takeValue = () => {
+  const selectedZone = selectedValue;
+  context.emit('update:modelValue', selectedZone);
+}
 </script>
